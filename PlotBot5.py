5b468419b91170b1cd92694d323fe8f9741c2939
Stephanie Armenta <40255500+sarmenta91@users.noreply.github.com>
2018-12-04 23:55:57 -0600

    Update PlotBot5.py

--- PlotBot5.py
+++ PlotBot5.py
@@ -1,16 +1,3 @@
-# coding: utf-8
-
-# ## PlotBot5.py
-# ----------------------------------------------------------------------------
-# The below code is for a Python based Twitter Bot.
-# The bot receives tweets via mentions and in turn performs sentiment analysis
-# on the most recent twitter account specified in the mention (i.e. A user
-# tweets: "@PlotBot Analyze: @CNN" and in turn the code will trigger sentiment
-# analysis on the CNN twitter feed). A plot from the sentiment analysis is
-# then tweeted to the PlotBot5 twitter feed. Notable libraries used to
-# complete this application include: Matplotlib, Pandas, and Tweepy.
-# ----------------------------------------------------------------------------
-
 # Dependencies
 import numpy as np
 import pandas as pd
